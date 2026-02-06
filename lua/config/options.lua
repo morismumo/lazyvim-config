35db7c8784b82655ce4b1fb44365065d848a7df3
morismumo <morismumo28@gmail.com>
2026-02-07 00:54:04 +0300

    configurations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,7 @@
+-- Sets the python path for the Neovim provider (useful for pynvim)
 vim.g.python3_host_prog = vim.fn.expand("~/.local/share/nvim/venv/bin/python")
+
+-- configure the Python Extra (requirs lang.python extra enabled)
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
@@ -7,3 +10,8 @@ vim.g.lazyvim_python_ruff = "ruff"
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
+
+vim.opt.runtimepath:append("/home/masua/.local/share/nvim/site")
+
+-- Required: Enable the language server
+vim.lsp.enable("ty")
